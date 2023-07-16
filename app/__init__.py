5f304a899a4eb90b5a319b469014b56018a9fb06
Lusca <toy.lcdv@hotmail.com>
2023-07-16 02:40:31 -0300

    apagando uma função que era pra teste

--- app/__init__.py
+++ app/__init__.py
@@ -16,26 +16,9 @@ migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
 
-from app.models.tables import teste
+
 from app.controllers import default
 from app.models import tables
 
 
 
-
-
-# Chamando a função para criar o objeto dentro do contexto da aplicação
-
-
-def create_user():
-    with app.app_context():
-        Rafaela = tables.User('rafaella', 'rafael@kska', '123a', 'Rafaela', 0)
-        db.session.add(Rafaela)
-        db.session.commit()
-
-#usuário: “pedro”, email: “pedro@unb.br”, senha: “asdfg”, perfil: professor
-#usuário: “ester”, email: “ester@unb.br” senha: “asdfg”, perfil: estudante
-#create_user()  
-#user = db.session.execute(db.select(tables.User).filter_by(id=1)).scalar_one()
-#db.session.delete(user)
-#db.session.commit() 
